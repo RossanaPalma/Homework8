17b32f5ce93207a2c86af72c9c151e3afb910e86
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2020-11-02 23:37:44 -0800

    Add project files.

---
+++ Project1/Duelist.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    class Duelist
+    {
+        private String _name;
+        private double _accuracy;
+        private bool _alive;
+        private Random rand;
+
+
+        public Duelist(string name) // constructor 
+        {
+            _name = name;
+        }
+
+        public Duelist(double accuracy) // constructor
+        {
+            _accuracy = accuracy;
+        }
+                
+
+        public string Name { get => _name; set => _name = value; }
+        public double Accuracy { get => _accuracy; set => _accuracy = value; }
+        public bool Alive { get => _alive; set => _alive = value; }
+
+        public void ShootAtTarget(Duelist duel)
+        {
+            if(rand.NextDouble() < _accuracy)
+            {
+                duel.Alive = false;
+            }
+        }
+
+    }
+    }
+
+
---
+++ Project1/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aaronWins, bobWins, charlieWins = 0;
+            
+            for(int i = 0; i < 10000; i++)
+            {
+                Duelist aaron = new Duelist("Aaron", 0.33);
+                Duelist bob = new Duelist("Bob", 0.5);
+                Duelist charlie = new Duelist("Charlie", 0.995);
+                int count 3; // shooters alive
+
+                while (count > 1)
+                {
+                    if (aaron.Alive)
+                    {
+                        if (charlie.Alive)
+                        {
+                            aaron.ShootAtTarget(charlie);
+                            if (!charlie.Alive)
+                            {
+                                count--;
+                            }
+                        }
+                        else
+                        {
+                            aaron.ShootAtTarget(bob)
+                                if (!bob.Alive)
+                            {
+                                count--;
+                            }
+                        }
+                    }
+                    if (bob.Alive)
+                    {
+                        if (charlie.Alive)
+                        {
+                            bob.ShootAtTarget(charlie);
+                            if (!charlie.Alive)
+                            {
+                                count--;
+                            }
+                        }
+                        else
+                        {
+                            bob.ShootAtTarget(aaron);
+                            if (aaron.Alive)
+                            {
+                                aaronWins++;
+                            }
+                        }
+                    }
+                    if (charlie.Alive)
+                    {
+                        if (bob.Alive)
+                        {
+                            charlie.ShootAtTarget(bob);
+                            if (!bob.Alive)
+                            {
+                                count--;
+                            }
+                            else
+                            {
+                                charlie.ShootAtTarget(aaron);
+                                if (!aaron.Alive)
+                                {
+                                    count--;
+                                }
+                            }
+                        }
+                    }
+                    if (aaron.Alive)
+                    {
+                        aaronWins++;
+                    }
+                    else if (bob.Alive)
+                    {
+                        bobWins++;
+                    }
+                    else
+                    {
+                        charlieWins++;
+                    }
+                }
+                Console.WriteLine("Aaron won {0} times.", aaronWins);
+                Console.WriteLine("Bob won {0} times,", bobWins);
+                Console.WriteLine("Charlie won {0} times.", charlieWins);
+            }
+            Console.ReadLine();
+            }
+        }
+    }
+
